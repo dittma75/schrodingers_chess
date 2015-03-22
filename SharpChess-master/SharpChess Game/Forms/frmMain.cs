90e3cddcc7afe79cc6340f405332dd9611d2e430
Kevin Dittmar <dittma75@students.rowan.edu>
2015-03-22 14:14:55 -0400

    Started Concealed Additions
    Added cursors and images for Concealed pieces.
    Concealed pieces currently show up as Rooks.
    Added concealed pieces to the resources path, but the game crashes
    (commented these lines for now).

--- SharpChess-master/SharpChess Game/Forms/frmMain.cs
+++ SharpChess-master/SharpChess Game/Forms/frmMain.cs
@@ -95,6 +95,7 @@ namespace SharpChess
         /// The m_acur piece cursors.
         /// </summary>
         private readonly Cursor[] m_acurPieceCursors = new Cursor[12];
+        //private readonly Cursor[] m_acurPieceCursors = new Cursor[14];
 
         /// <summary>
         /// The m_form move analysis.
@@ -2075,6 +2076,9 @@ namespace SharpChess
             this.m_acurPieceCursors[9] = new Cursor(asmMain.GetManifestResourceStream(strPath + "WhitePawn.cur"));
             this.m_acurPieceCursors[10] = new Cursor(asmMain.GetManifestResourceStream(strPath + "BlackQueen.cur"));
             this.m_acurPieceCursors[11] = new Cursor(asmMain.GetManifestResourceStream(strPath + "WhiteQueen.cur"));
+/*          this.m_acurPieceCursors[12] = new Cursor(asmMain.GetManifestResourceStream(strPath + "BlackConcealed.cur"));
+            this.m_acurPieceCursors[13] = new Cursor(asmMain.GetManifestResourceStream(strPath + "WhiteConcealed.cur"));
+ */
         }
 
         /// <summary>
